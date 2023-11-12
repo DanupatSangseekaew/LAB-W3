1dd8ac92ae27711ae685b4652bd714757f358134
DanupatSangseekaew <64030059@kmitl.ac.th>
2023-11-13 03:39:37 +0700

    first commit

---
+++ main/LED.h
@@ -0,0 +1,15 @@
+/*
+ * LED.h
+ *
+ *  Created on: Aug 22, 2023
+ *      Author: koson
+ */
+
+#ifndef MAIN_LED_H_
+#define MAIN_LED_H_
+
+void LED_Init();
+void LED_On();
+void LED_Off();
+
+#endif /* MAIN_LED_H_ */
